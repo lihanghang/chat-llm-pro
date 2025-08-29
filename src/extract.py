f6e80f350556d6f32c9184d3c058542506c737f4
lihanghang <lhhroots@126.com>
2025-08-29 23:41:33 +0800

    refactor(src): 重构 extract.py 文件
    - 更新代码格式，使其更加规范和可读
    - 修改日志配置，增加可读性
    - 更新 MyModal 类的实现，使用新的请求方式
    - 优化 pdf 解析和文档抽取相关函数

--- src/extract.py
+++ src/extract.py
@@ -3,6 +3,7 @@
 https://python.langchain.com/en/latest/modules/indexes/document_loaders/examples/unstructured_file.html
 https://eyurtsev.github.io/kor/   extract structured data from text using large language models (LLMs) 🧩.
 """
+
 import asyncio
 import json
 import logging
@@ -22,8 +23,11 @@ import dotenv
 from langchain.chat_models import AzureChatOpenAI
 
 logger = logging.getLogger()
-logging.basicConfig(level=logging.INFO, format='[%(pastime)s] {%(pathname)s:%(lineno)d} %(levelness)s - %(message)s',
-                    datefmt='%H:%M:%S')
+logging.basicConfig(
+    level=logging.INFO,
+    format="[%(pastime)s] {%(pathname)s:%(lineno)d} %(levelness)s - %(message)s",
+    datefmt="%H:%M:%S",
+)
 logger.setLevel(logging.INFO)
 
 config = dotenv.dotenv_values(".env")
@@ -32,35 +36,43 @@ openai.api_base = config["OPENAI_API_BASE"]
 openai.api_version = config["OPENAI_API_VERSION"]
 
 # fix 尽量用这种方式设置azure的可以，测试了下openai_api_key不起作用。
-os.environ['OPENAI_API_KEY'] = config["AZURE_OPENAI_API_KEY"]
-endpoint_url = os.getenv('MEM_FIN_OPENAI_API')
+os.environ["OPENAI_API_KEY"] = config["AZURE_OPENAI_API_KEY"]
+endpoint_url = os.getenv("MEM_FIN_OPENAI_API")
 
 
 azure_llm = AzureChatOpenAI(
-        deployment_name="gpt-35-turbo",
-        temperature=0,
-        max_tokens=2000,
-        frequency_penalty=0,
-        presence_penalty=0,
-        top_p=1.0
+    deployment_name="gpt-35-turbo",
+    temperature=0,
+    max_tokens=2000,
+    frequency_penalty=0,
+    presence_penalty=0,
+    top_p=1.0,
 )
 
 
 class MyModal(Modal):
 
     def _call(self, prompt: str, stop=None) -> str:
-        body = {"prompt": prompt, "max_length": 2048, "temperature": 0.2}
-        response = requests.post(self.endpoint_url, json=body)
-        return response.json()['response']
+        body = {
+            "model": "OxW/Qwen3-0.6B-GGUF",
+            "messages": [
+                {"role": "system", "content": "You are a helpful assistant."},
+                {"role": "user", "content": f"{prompt}!"},
+            ],
+        }
+
+        # body = {"prompt": prompt, "max_length": 2048, "temperature": 0.2}
+        response = requests.post(endpoint_url, json=body)
+        return response.json()["choices"][0]["message"]["content"]
 
 
 def parser_pdf(file_path) -> List[Document]:
     logging.info("加载并解析文件……")
     file_name = file_path.name
     hashcode = hashcode_with_file(file_name)
-    parser_file = parser_doc(file_name, f'data/store/{hashcode}')
+    parser_file = parser_doc(file_name, f"data/store/{hashcode}")
     contents: list = read_file(parser_file)
-    txt = ''.join(contents)
+    txt = "".join(contents)
     docs = [Document(page_content=txt)]
     return docs
 
@@ -70,19 +82,18 @@ def extract_doc(schema, llm_type, docs=[]):
     基于大模型进行文档信息抽取
     """
     mem_llm = MyModal(endpoint_url=endpoint_url)
-    llm_obj = mem_llm if llm_type == 'memect' else azure_llm            # azure_llm
+    llm_obj = mem_llm if llm_type == "memect" else azure_llm  # azure_llm
     logging.info("Create extract chain")
-    extraction_chain = create_extraction_chain(llm_obj, schema, encoder_or_encoder_class='json')
+    extraction_chain = create_extraction_chain(
+        llm_obj, schema, encoder_or_encoder_class="json"
+    )
     logging.info("Start extract from doc……")
     extraction_results = asyncio.run(
         extract_from_documents(
-            chain=extraction_chain,
-            documents=docs,
-            use_uid=False,
-            max_concurrency=2
+            chain=extraction_chain, documents=docs, use_uid=False, max_concurrency=2
         )
     )
-    ret = json.dumps(extraction_results[0]['data'], ensure_ascii=False, indent=4)
+    ret = json.dumps(extraction_results[0]["data"], ensure_ascii=False, indent=4)
     logging.info(ret)
     return ret
 
@@ -92,13 +103,13 @@ def chat_mem_fin_llm(endpoint_url, input_text, task_type):
     基于langchain调用memect LLM openapi
     """
     mem_llm = MyModal(endpoint_url=endpoint_url)
+    logging.info(f"Load model {endpoint_url}")
     query = f"{prompt_text[task_type]} {input_text.strip()}"
-    prompt_template = PromptTemplate(input_variables=["query"],
-                                     template=f'{{query}}')
+    prompt_template = PromptTemplate(input_variables=["query"], template=f"{{query}}")
     llm_chain = LLMChain(llm=mem_llm, prompt=prompt_template)
     response = llm_chain.run(query)
     return response
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     pass
